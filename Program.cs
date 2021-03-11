8c3d2c36d91280db9612400e54351609bffceefc
felipefs <felipe.fs.vr@gmail.com>
2021-03-11 06:03:05 -0300

    Início

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DIO.CadastroSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cadastro de Series");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+### Projeto: 
+
+**Cadastro de Series em .Net **
+
+Site: [Digital Innovation One](http://www.digitalinnovation.one/)
